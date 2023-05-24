44b459366f10c202cec40662079a4b0e8d3297da
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-24 13:00:54 +0530

    chnages

--- game.php
+++ game.php
@@ -3,20 +3,278 @@
 include './components/header.php';
 ?>
 
-<div class="w-full h-full justify-center items-center flex mt-[20vh]">
-    <div class="w-full max-w-[30%] p-4 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-600 dark:border-gray-700">
+<div class="w-full h-full justify-center items-center flex mt-10">
+
+    <div class="w-full max-w-[90%] p-1 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-600 dark:border-gray-700">
         <form class="space-y-6" action="#">
-            <h5 class="text-xl font-small text-gray-900 dark:text-white">Working Date</h5>
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Validating Working Date</label>
-                <input type="date" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Table
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Details
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Team 1
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Team 2
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Result
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Microsoft Surface Pro
+                            </th>
+                            <td class="px-6 py-4">
+                                White
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop PC
+                            </td>
+                            <td class="px-6 py-4">
+                                No
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                $1999
+                            </td>
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Magic Mouse 2
+                            </th>
+                            <td class="px-6 py-4">
+                                Black
+                            </td>
+                            <td class="px-6 py-4">
+                                Accessories
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                No
+                            </td>
+                            <td class="px-6 py-4">
+                                $99
+                            </td>
+
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple Watch
+                            </th>
+                            <td class="px-6 py-4">
+                                Black
+                            </td>
+                            <td class="px-6 py-4">
+                                Watches
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                No
+                            </td>
+                            <td class="px-6 py-4">
+                                $199
+                            </td>
+
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple iMac
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                PC
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple AirPods
+                            </th>
+                            <td class="px-6 py-4">
+                                White
+                            </td>
+                            <td class="px-6 py-4">
+                                Accessories
+                            </td>
+                            <td class="px-6 py-4">
+                                No
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                $399
+                            </td>
+
+
+                        </tr>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="w-4 p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </td>
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                iPad Pro
+                            </th>
+                            <td class="px-6 py-4">
+                                Gold
+                            </td>
+                            <td class="px-6 py-4">
+                                Tablet
+                            </td>
+                            <td class="px-6 py-4">
+                                No
+                            </td>
+                            <td class="px-6 py-4">
+                                Yes
+                            </td>
+                            <td class="px-6 py-4">
+                                $699
+                            </td>
+
+
+                        </tr>
+                        
+                    </tbody>
+                </table>
             </div>
-            <div class="flex justify-end">
-                <button type="submit" class="w-[100px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-2">Ok</button>
-                <button type="submit" class="w-[100px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+
+
+        </form>
+
+        <div class="justify-between mt-5">
+            <div class="flex justify-between">
+                <div class="w-full">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                    <input type="date" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm input-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
+                <div class="w-full ml-10">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Details</label>
+                    <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm input-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
             </div>
+            <div class="flex justify-between mt-5">
+                <div class="w-full">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                    <input type="number" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm input-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
+                <div class="w-full ml-10">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Team 1</label>
+                    <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm input-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
+                <div class="w-full ml-10">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Team 2</label>
+                    <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm input-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                </div>
+            </div>
+            <div class="flex mt-5 justify-start">
+                <div>
+                    
+                    <button class="btn rounded-none btn-sm btn-wide btn-primary">
+                        Save
+                    </button>
+                </div>
+                <div class="ml-10">
+                    
+                    <button class="btn rounded-none btn-sm btn-wide btn-primary">
+                        Reset
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
 
 </body>
 
