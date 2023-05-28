d005a2d7f5f7043368300e33ab3206be0972a609
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-28 14:09:17 +0530

    jdfjg

--- customer.php
+++ customer.php
@@ -19,7 +19,6 @@ $result = $conn->query($sql);
 <div class="w-full h-full justify-center items-center flex mt-[5vh]">
     <div class="w-full max-w-[80%] p-4 bg-white border rounded-lg border-gray-200 shadow sm:p-6 md:p-8 dark:bg-gray-600 dark:border-gray-700">
         <form class="space-y-6" action="./backend/customer.php" method="get">
-
             <div class="flex justify-between items-end w-full">
                 <div class="w-full mr-5">
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NAME</label>
@@ -42,70 +41,76 @@ $result = $conn->query($sql);
             </div>
             <button class="w-[150px] text-black btn-sm border-white-200 shadow h-10 pl-2 pr-2 btn btn-primary rounded btn-success btn-active" type="submit">Addittion</button>
             <div class="w-[150px] text-black btn-sm border-white-200 shadow h-10 pl-2 pr-2 btn btn-primary rounded btn-active" value="update" id="update">Modification</div>
-            <div class="w-[150px] text-black btn-sm border-white-200 shadow h-10 pl-2 pr-2 btn btn-secondary rounded btn-active" value="delete">Delete</div>
-
-            <div class="box-content h-[40vh] w-90 border-2 border-gray-100 bg-gray-100 overflow-y-scroll">
-                <div class="rounded-none">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="p-4">
-                                    <div class="flex items-center">
-
-                                    </div>
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Table
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Date
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Details
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Team 1
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="rounded-none">
-                            <?php
-                            if ($result->num_rows > 0) {
-                                $serialNumber = 1;
-                                // Output data of each row
-                                while ($row = $result->fetch_assoc()) {
-                                    $id = $row["id"]; // Assuming the ID column name is "id"
-                            ?>
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <td class="w-4 p-4">
-                                            <div class="flex items-center">
-                                                <input type="checkbox" value="<?php echo $id; ?>" class="checkbox checkbox-secondary" id="checkbox" name="selected_id[]" />
-                                            </div>
-                                        </td>
-                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            <?php echo $serialNumber; ?>
-                                        </th>
-                                        <td class="px-6 py-4">
-                                            <?php echo $row["name"]; ?>
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            <?php echo $row["reference"]; ?>
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            <?php echo $row["mobile"]; ?>
-                                        </td>
-                                    </tr>
-                            <?php $serialNumber++;
-                                }
-                            } else {
-                                echo "No data found";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <!-- <div class="w-[150px] text-black btn-sm border-white-200 shadow h-10 pl-2 pr-2 btn btn-secondary rounded btn-active" value="delete">Delete</div> -->
         </form>
+        <div class="box-content h-[40vh] w-90 border-2 border-gray-100 bg-gray-100 overflow-y-scroll mt-5">
+            <div class="rounded-none">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="p-4">
+                                <div class="flex items-center">
+
+                                </div>
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Table
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Details
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Team 1
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="rounded-none">
+                        <?php
+                        if ($result->num_rows > 0) {
+                            $serialNumber = 1;
+                            // Output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                $id = $row["id"]; // Assuming the ID column name is "id"
+                        ?>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td class="w-4 p-4">
+                                        <div class="flex items-center">
+                                            <input type="checkbox" value="<?php echo $id; ?>" class="checkbox checkbox-secondary" id="checkbox" name="selected_id[]" />
+                                        </div>
+                                    </td>
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <?php echo $serialNumber; ?>
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        <?php echo $row["name"]; ?>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <?php echo $row["reference"]; ?>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <?php echo $row["mobile"]; ?>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <!-- <?php echo $row["mobile"]; ?> -->
+                                        <button class="text-black btn-sm btn btn-secondary rounded btn-active">Delete</button>
+                                    </td>
+                                </tr>
+                        <?php $serialNumber++;
+                            }
+                        } else {
+                            echo "No data found";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -116,7 +121,7 @@ $result = $conn->query($sql);
 
     //On checkbox click
     selected_checkbox.addEventListener('change', () => {
-       console.log("Clicked")
+        console.log("Clicked")
     })
 
     // Update button click
