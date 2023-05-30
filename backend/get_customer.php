c4757045e09d16afb5f4cc6e5da3115004cb45ce
Harsh Singh <sujeetsingh734321@gmail.com>
2023-05-31 00:10:14 +0530

    chnages

--- backend/get_customer.php
+++ backend/get_customer.php
@@ -12,28 +12,24 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 $sql = "SELECT * FROM customer";
 $result = $conn->query($sql);
 
 // Check if any data was found
 if ($result->num_rows > 0) {
-    // Create an array to store the data
-    $data = array();
+    // Create a string to store the data
+    $text = "";
 
-    // Fetch the data and add it to the array
+    // Fetch the data and append it to the string
     while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
+        $text .= implode("\t", $row) . "\n";
     }
 
-    // Set the appropriate headers for JSON response
-    header('Content-Type: application/json');
+    // Set the appropriate headers for plain text response
+    header('Content-Type: text/plain');
 
-    // Convert the data array to JSON
-    $json = json_encode($data);
-
-    // Output the JSON data
-    echo $json;
+    // Output the plain text data
+    echo $text;
 } else {
     echo "No data found for the given name.";
 }
